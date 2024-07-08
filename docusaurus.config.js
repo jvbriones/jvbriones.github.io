0e3679b0d5d150a6164f0f2e86520872e516e8ba
Javier Briones <1674192+jvbriones@users.noreply.github.com>
2024-07-08 20:56:37 +0200

    chore: init setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           sidebarPath: './sidebars.js',
           showLastUpdateAuthor: false,
           showLastUpdateTime: true,
-          routeBasePath: '/',
+          // routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
