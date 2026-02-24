cb6691ced0713d3bcca4714e2d4524a23e6746aa
jvbriones <1674192+jvbriones@users.noreply.github.com>
2026-02-24 12:06:27 +0100

    chore: update project configuration and dependencies (#16)

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.config({
+    extends: ["plugin:@docusaurus/recommended"],
+    env: {
+      es6: true,
+    },
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: 2018,
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      "@docusaurus/no-untranslated-text": "off",
+      "@docusaurus/string-literal-i18n-messages": "warn",
+      "@docusaurus/no-html-links": "error",
+      "@docusaurus/prefer-docusaurus-heading": "error",
+      "no-console": ["error", { allow: ["error", "warn"] }],
+    },
+  }),
+];
